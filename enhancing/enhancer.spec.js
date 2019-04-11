2cb7d6e8fd32746c3fad19a7991dab91afeb1135
CatsAreMLG <joshuadgon@gmail.com>
2019-04-10 19:05:53 -0500

    Initial commit

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,8 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { succeed, fail, repair, get } = require('./enhancer.js')
+
+describe('Asynchronous tests', () => {
+  const result = succeed('h')
+  it('async using callback', () => {
+    expect(result).toBe('hi')
+  })
+})
